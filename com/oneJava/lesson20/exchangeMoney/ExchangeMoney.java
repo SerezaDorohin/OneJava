f2532995017291da53819da2a5dea70776a3214f
SerezaDorohin <serezadorohin@icloud.com>
2019-01-21 23:10:39 +0700

    -|21.01.2019|-

--- com/oneJava/lesson20/exchangeMoney/ExchangeMoney.java
+++ com/oneJava/lesson20/exchangeMoney/ExchangeMoney.java
@@ -2,20 +2,18 @@ package com.oneJava.lesson20.exchangeMoney;
 
 /**
  * @author SergeyDorohin;
- * @version 1.2;
+ * @version 1.2(IT IS NOT WORKING);
  * @task Избавиться от переменной indexOfCoin;
  * @type Домашняя работа;
  */
 
 public class ExchangeMoney {
     private static int[] COINS_NOM = {1, 5, 10, 25, 50};
+
     public static int getCountOfWays(int money) {
         return getCountOfWays(money, 4);
     }
     private static int getCountOfWays(int money, int indexOfCoin) {
-        if(!(money > 0)) {
-            return 0;
-        }
         if (money < 0 || indexOfCoin < 0) return 0;
         if (money == 0 || indexOfCoin == 0) return 1;
         return getCountOfWays(money, indexOfCoin - 1) + getCountOfWays(money - COINS_NOM[indexOfCoin], indexOfCoin);
