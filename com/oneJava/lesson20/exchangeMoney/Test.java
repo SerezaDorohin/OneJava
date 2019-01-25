75c0c14274d3b815518f9f9681fb74c13674ceaf
SerezaDorohin <serezadorohin@icloud.com>
2019-01-25 23:51:14 +0700

    -|25.01.2019|-

--- com/oneJava/lesson20/exchangeMoney/Test.java
+++ com/oneJava/lesson20/exchangeMoney/Test.java
@@ -2,6 +2,10 @@ package com.oneJava.lesson20.exchangeMoney;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(ExchangeMoney.getCountOfWays(100));
+        System.out.println("coins: 1, 2, 5, 10, 20, 40.");
+        System.out.println("money: " + 1 + ", answer: " + ExchangeMoney.getCountOfWays(1) + ".");
+        System.out.println("money: " + 2 + ", answer: " + ExchangeMoney.getCountOfWays(2) + ".");
+        System.out.println("money: " + 5 + ", answer: " + ExchangeMoney.getCountOfWays(5) + ".");
+        System.out.println("money: " + 100 + ", answer: " + ExchangeMoney.getCountOfWays(100) + ".");
     }
 }
