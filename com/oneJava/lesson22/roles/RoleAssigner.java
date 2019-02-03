c289b2b64b738180759752113f1242c2fe1998dd
SerezaDorohin <serezadorohin@icloud.com>
2019-02-03 16:49:25 +0700

    -|03.02.2019|-

---
+++ com/oneJava/lesson22/roles/RoleAssigner.java
@@ -0,0 +1,30 @@
+package com.oneJava.lesson22.roles;
+
+public class RoleAssigner {
+    String printTextPerRole(String[] roles, String[] textLines) {
+        StringBuilder end = new StringBuilder();
+        String[] authors = new String[textLines.length];
+        int[] pos = new int[textLines.length];
+        for(int i = 0; i < textLines.length; i++) {
+            authors[i] = textLines[i].split(":")[0];
+            pos[i] = i + 1;
+        }
+        for(int i = 0; i < roles.length; i++) {
+            for (int n = 0; n < authors.length; n++) {
+                if (roles[i].equals(authors[n])) {
+                    end.append(roles[i] + ":\n");
+                    for (int l = 0; l < textLines.length; l++) {
+                        if (textLines[l].startsWith(authors[n] + ":")) {
+                            end.append(pos[l] + ")" + textLines[l].substring(roles[i].length() + 1) + "\n");
+                        }
+                    }
+                    if(!((i + 1) == roles.length)) {
+                        end.append("\n");
+                    }
+                    break;
+                }
+            }
+        }
+        return end.toString();
+    }
+}
---
+++ com/oneJava/lesson22/roles/Test.java
@@ -0,0 +1,27 @@
+package com.oneJava.lesson22.roles;
+
+public class Test {
+    private RoleAssigner roleAssigner;
+
+    public static void main(String[] args) {
+        new Test().start();
+    }
+
+    public void start() {
+        roleAssigner = new RoleAssigner();
+        String[] roles = {
+                "Городничий", "Аммос Федорович",
+                "Артемий Филиппович",
+                "Лука Лукич"};
+        String[] textLines = {
+                "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+                "Аммос Федорович: Как ревизор?",
+                "Артемий Филиппович: Как ревизор?",
+                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+                "Аммос Федорович: Вот те на!",
+                "Артемий Филиппович: Вот не было заботы, так подай!",
+                "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
+        String output = roleAssigner.printTextPerRole(roles, textLines);
+        System.out.println(output);
+    }
+}
